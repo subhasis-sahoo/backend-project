726815d6300c2a80cb3c56f64fe98aaac5761e79
subhasis-sahoo <subhasissahoo949@gmail.com>
2025-01-31 11:22:19 +0530

    connect to mongo atlas

---
+++ src/index.js
@@ -0,0 +1,48 @@
+// require('dotenv').config({path: './env'});
+import dotenv from "dotenv";
+import connectDB from "./db/index.js";
+
+dotenv.config({
+    path: './env'
+})
+
+
+connectDB()
+
+
+
+
+
+
+
+/*
+import express from "express";
+const app = express();
+
+// Here we use ';' to ensureing that this IFFI function is write on the new line.
+;( async() => {
+    try {
+        await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+
+        app.on("error", (error) => {
+            console.log("ERROR: ", error);
+            throw error;
+        })
+
+        app.listen(process.env.PORT, () => {
+            console.log(`App is listening on port ${process.env.PORT}`);
+        })
+
+    } catch(error) {
+        console.error("ERROR: ", error)
+        throw error
+    }
+} )()
+*/
+
+
+/*
+NOTES: While connecting database remember two things,
+i-> There is always may some error occur while connecting database.
+ii-> Database takes a lot of time to connect, so always use async and await.
+*/
